e288c62e8033723c9b650255b8cd0398e9023a6a
Marchinho11 <egoroff-ars@yandex.ru>
2016-12-11 14:49:40 +0300

    Edit markup

--- www/static/js/info.js
+++ www/static/js/info.js
@@ -14,7 +14,6 @@ function loadAnalytics() {
 function renderAnalytics(data){
     document.getElementById('image').innerHTML ="<img class = 'rounded' src='" + data['response'][0]['photo_100']+ "' />";
     document.getElementById('name_of_public').innerHTML="<h3>" + data['response'][0]['name'] + "</h3>";
-    document.getElementById('about').innerHTML= data['response'][0]['description'];
     document.getElementById("ours").innerHTML = "Ваше сообщество";
 
     var sexData = [
